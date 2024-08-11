abf09e0048a3a6d4832f9fc8a475631488d891eb
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-08-11 19:38:33 +0100

    add models

---
+++ WebAppServer/Models/AccidentStatsModels.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppServer.Models
+{
+
+    public class AccidentStats
+    {
+        public required List<AccidentStatsDetail> accidentStats { get; set; }
+    }
+
+    public class AccidentStatsDetail
+    {
+        public int id { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string location { get; set; }
+        public string date { get; set; }
+        public string severity { get; set; }
+        public string borough { get; set; }
+        public List<AccidentStatsCasualty> casualities { get; set; } 
+        public List<AccidentStatsVehicle> vehicles { get; set; }
+    }
+    public class AccidentStatsCasualty
+    {
+        public int age { get; set; }
+        public string className { get; set; }
+        public string severity { get; set; }
+        public string mode { get; set; }
+        public string ageBand { get; set; } 
+    }
+    public class AccidentStatsVehicle
+    {
+        public string type { get; set; }
+    }
+}
---
+++ WebAppServer/Models/BikePointModels.cs
@@ -0,0 +1,32 @@
+namespace WebAppServer.Models
+{
+
+    public class BikePoints
+    {
+        public required List<Place> places { get; set; }
+    }
+    public class Place
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public string commonName { get; set; }
+        public double distance { get; set; }
+        public string placeType { get; set; }
+        public List<AdditionalProperty> additionalProperties { get; set; }
+        public List<Place> children { get; set; }
+        public string[] childrenUrls { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+
+    }
+
+    public class AdditionalProperty
+    {
+        public string category { get; set; }
+        public string key { get; set; }
+        public string sourceSystemKey { get; set; }
+        public string value { get; set; }
+        public string modified { get; set; }
+    }
+
+}
